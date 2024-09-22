fd5f9897cb300e5f8cd1d5378501465855660403
Cripsy01 <Chris1203.Gerritsen@gmail.com>
2024-09-22 21:10:30 +0200

    third person camera switch

--- Player.gd
+++ Player.gd
@@ -11,18 +11,37 @@ const BOB_AMP = 0.08
 var t_bob = 0.0
 
 @onready var head: Node3D = $Head
-@onready var camera: Camera3D = $Head/Camera3D
+@onready var camera1: Camera3D = $Head/Camera3D
+@onready var camera2: Camera3D = $Head/Camera3D2
+var active_camera: Camera3D
 
 
 func _ready() -> void:
+	active_camera = camera1
+	camera1.current = true
+	camera2.current = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("switch_camera"):
+		toggle_camera()
+		
+func toggle_camera():
+	if active_camera == camera1:
+		active_camera = camera2
+		camera1.current = false
+		camera2.current = true
+	else:
+		active_camera = camera1
+		camera1.current = true
+		camera2.current = false
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		active_camera.rotate_x(-event.relative.y * SENSITIVITY)
+		active_camera.rotation.x = clamp(active_camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 	
 
 func _physics_process(delta: float) -> void:
@@ -47,7 +66,7 @@ func _physics_process(delta: float) -> void:
 
 	# Head bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
+	active_camera.transform.origin = _headbob(t_bob)
 
 	move_and_slide()
 
