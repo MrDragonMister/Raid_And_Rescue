890d07be446e33126b356ec2aa6934365c5a04b3
MrDragonMister <milanvansomeren.panda@outlook.com>
2024-11-07 17:47:26 +0100

    neater healthbar code

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -9,16 +9,12 @@ func _ready():
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
 		for n in 10:
-			health -= 1
-			if health <= health_bar.min_value:
-				health = health_bar.min_value
+			health = max(health - 1, health_bar.min_value)
 			health_bar.value = health
 			await get_tree().create_timer(0.01).timeout
 	if Input.is_action_just_pressed("interact"):
 		for n in 10:
-			health += 1
-			if health >= health_bar.max_value:
-				health = health_bar.max_value
+			health = min(health + 1, health_bar.max_value)
 			health_bar.value = health
 			await get_tree().create_timer(0.01).timeout
 		
