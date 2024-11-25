c926eb737c78c8fe0c87c48e7502cb75c43d2b67
Cripsy01 <Chris1203.Gerritsen@gmail.com>
2024-11-25 21:06:13 +0100

    basic combat

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -17,28 +17,15 @@ func _process(delta: float) -> void:
 	if camera:
 		# Rotate the Sprite3D to face the camera's position
 		enemy_health_display.look_at(camera.global_transform.origin, Vector3.UP)
-
-	if Input.is_action_just_pressed("attack"):
-		if health > health_bar.min_value:
-			for n in 10:
-				health -= 1
-				health_bar.value = health
-				await get_tree().create_timer(0.01).timeout
-	if Input.is_action_just_pressed("interact"):
-		if health < health_bar.max_value:
-			for n in 10:
-				health += 1
-				health_bar.value = health
-				await get_tree().create_timer(0.01).timeout
 				
-	var player = get_node("../Player")
+	var player = $"../../Player"
 	var player_position = player.global_transform.origin
 	var enemy_position = global_transform.origin
-	var playerdir_to_enemy = player.direction_to(enemy)
+	var playerdir_to_enemy = player_position.direction_to(enemy_position)
 	var playerinput_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
 	var playerdir: Vector3 = (player.transform.basis * Vector3(playerinput_dir.x, 0, playerinput_dir.y)).normalized()
 	if Input.is_action_just_pressed("attack"):
-		if enemy_position.distance_to(player_position) <= 3:
+		if enemy_position.distance_to(player_position) <= 5:
 			if playerdir_to_enemy.dot(playerdir):
 				for n in 10:
 					health = max(health - 1, health_bar.min_value)
