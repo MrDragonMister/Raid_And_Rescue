c806830d5fca6e24dc854d7660cbc609288a1dca
MrDragonMister <milanvansomeren.panda@outlook.com>
2024-12-12 21:08:29 +0100

    old changes re-added

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -1,6 +1,15 @@
 extends ProgressBar
 
+@onready var health_bar_text: RichTextLabel = $RichTextLabel
+
+func _ready():
+	update_health_bar_text()
+
 func _process(_delta):
 	#self.value = Global.amount_of_enemies
 	pass
+	
+func update_health_bar_text():
+	health_bar_text.clear()
+	health_bar_text.append_text("[center]%s/%s" % [self.value, self.max_value])
 	
