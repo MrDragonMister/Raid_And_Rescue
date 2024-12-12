9bf5820b351219eccfd645da16ef9969320b7f75
MrDragonMister <milanvansomeren.panda@outlook.com>
2024-12-12 23:35:23 +0100

    the mouse shows when entering the main menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,7 +3,8 @@ extends Node2D
 @onready var death = $death
 
 func _ready():
-	death.play()
+	# death.play()
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
