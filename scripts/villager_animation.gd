6aeac0b778a4d25631c1f4edd6e1330e7c99c8ac
not-a-viking <15717@csvvg.eu>
2025-03-13 20:53:41 +0100

    Stijn updates
    Arrow
    Cage
    Gate
    Villager2
    Arrow scene
    House scene kleur
    intro
    level 1 aangepast
    level 2-alfred
    villager texture
    villager animation
    well scene
    alfred toevoegen
    axe toevoegen + animation
    bow toevoegen

--- scripts/villager_animation.gd
+++ scripts/villager_animation.gd
@@ -1,6 +1,6 @@
 extends AnimationPlayer
 
-@onready var villager = $"../.."
+@onready var villager = $".."
 
 func _process(_delta: float) -> void:
 	if not is_playing() and villager.velocity.length() > 2:
